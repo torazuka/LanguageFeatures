8f414ccba3b36fb6245bd2ac2b17a46ba007601b
torazuka <torazuka@gmail.com>
2013-08-01 18:12:51 +0900

    added properties in the Product.cs

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -7,11 +7,27 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        private int productID;
         private string name;
+        private string description;
+        private decimal price;
+        private string category;
 
+        public int ProductID {
+            get { return productID; }
+            set { productID = value; }
+        }
+        
         public string Name { 
             get { return name; }
             set { name = value;  }
         }
+
+        public string Description {
+            get { return description; }
+            set { description = value; }
+        }
+
+        // ... つづく
     }
 }
