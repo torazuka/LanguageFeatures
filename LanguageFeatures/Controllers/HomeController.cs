92793c1b020ead82a3382ba3bb9ffcab77b2cf89
torazuka <torazuka@gmail.com>
2013-08-01 17:29:40 +0900

    added the AutoProperty method

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LanguageFeatures.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,15 @@ namespace LanguageFeatures.Controllers
         public string Index()
         {
             return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty() {
+            Product myProduct = new Product();
+
+            myProduct.Name = "Kayak";
+            string productName = myProduct.Name;
+            
+            return View("Result", (object)String.Format("Product name: {0}", productName));
         }
 
     }
