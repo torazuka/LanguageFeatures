7dc1d5f434b7c2d791c187b16d49cbee1ff9db11
torazuka <torazuka@gmail.com>
2013-08-01 18:22:27 +0900

    changed the name property's logic

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -7,8 +7,19 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        private string name;
+
         public int ProductID { get; set; }
-        public string Name { get; set;}
+
+        public string Name {
+            get { 
+                return ProductID + name;
+            }
+            set { 
+                name = value;
+            }
+        }
+
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
