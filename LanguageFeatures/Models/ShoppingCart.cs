73e6059295e1d973149755dd9f0def537f81b0f9
torazuka <torazuka@gmail.com>
2013-08-02 11:14:52 +0900

    added comments to GetEnumerable methods

--- LanguageFeatures/Models/ShoppingCart.cs
+++ LanguageFeatures/Models/ShoppingCart.cs
@@ -10,13 +10,16 @@ namespace LanguageFeatures.Models
     {
         public List<Product> Products { get; set; }
 
+        // IEnumerable<T>インタフェースの実装
         public IEnumerator<Product> GetEnumerator() {
             return Products.GetEnumerator();
         }
 
+        // IEnumerableインタフェースの実装．IE<T>との区別のため，インタフェース名をつける．
+        // インタフェースを介してのみ呼び出せる．
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return GetEnumerator();
+            return GetEnumerator(); // IE<T>を返すGetEnumeratorを呼び出す．
         }
     }
 }
