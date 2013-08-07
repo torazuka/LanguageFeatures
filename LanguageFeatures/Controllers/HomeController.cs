baecebea05923b6854da769fa7f996c651f7110d
torazuka <torazuka@gmail.com>
2013-08-07 13:47:57 +0900

    changed a lambda expression as a parameter to the Filter method

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -101,11 +101,9 @@ namespace LanguageFeatures.Controllers
                 }
             };
 
-            Func<Product, bool> categoryFilter = prod => prod.Category == "Soccer";
-
             decimal total = 0;
 
-            foreach(Product prod in products.Filter(categoryFilter)){
+            foreach(Product prod in products.Filter(prod => prod.Category == "Soccer")){
                 total += prod.Price;
             }
 
