3961fe83d505b9c69b6433dd6ee71debe00074e2
torazuka <torazuka@gmail.com>
2013-08-08 10:12:18 +0900

    added an async method

---
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpTask = client.GetAsync("http://appres.com/");
+
+            // HTTPリクエストが完了するまで他のことができる
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) => {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+    }
+}
